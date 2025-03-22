45395c5e96a0c32b354d31733c91d61c97bcfd93
m-ishizaki <love.ms.is12t@hotmail.co.jp>
2025-03-22 23:35:23 +0900

    wip

---
+++ src/PUtilityCommands/PUtilityCommands/Addon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RKSoftware.PUtilityCommands
+{
+    public class Addon : RKSoftware.IPUtilities.AddOns.IAddon
+    {
+        public string Name => "PUtilityCommands";
+        public string Command => "putilitycommands";
+        public string Description => "A collection of utility commands for the PUtilities application.";
+        public string Version => "1.0.0";
+        public void Initialize()
+        {
+        }
+        public void Execute(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: PUtilities putilitycommands rds sqlserver");
+                return;
+            }
+
+            if (string.Compare("rds", args[0], true) == 0)
+                if (string.Compare("sqlserver", args[1], true) == 0)
+                {
+                    new Commands.RdsConfig().Execute().Wait();
+                    return;
+                }
+
+            Console.WriteLine("Invalid command.");
+        }
+    }
+}
---
+++ src/PUtilityCommands/PUtilityCommands/Commands/RdsConfig.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RKSoftware.PUtilityCommands.Commands
+{
+    internal class RdsConfig
+    {
+        Lazy<HttpClient> httpClient = new Lazy<HttpClient>(() => new HttpClient());
+        public async Task Execute()
+        {
+            var test = await httpClient.Value.GetAsync("https://raw.githubusercontent.com/pleasanter-developer-community/Implem.Pleasanter/6d57a986218bf39d58e572189ff15f7bfe1b8a8a/Implem.Pleasanter/App_Data/Parameters/Rds.json");
+            var result = await test.Content.ReadAsStringAsync() ?? string.Empty;
+            dynamic jsonObject = Newtonsoft.Json.JsonConvert.DeserializeObject(result);
+            jsonObject.Dbms = "SQLServer";
+            var jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObject);
+            File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "Rds.json"), jsonString);
+        }
+    }
+}
